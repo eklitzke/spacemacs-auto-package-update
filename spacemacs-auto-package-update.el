87f5fbfe77dba204880ea02c1513a359b6d06c04
Evan Klitzke <evan@eklitzke.org>
2021-05-20 01:09:08 -0700

    clean this up

--- spacemacs-auto-package-update.el
+++ spacemacs-auto-package-update.el
@@ -13,29 +13,28 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defvar spacemacs-auto-package/update-age 86400
+(defvar spacemacs-auto-package-update/age 86400
   "Age in seconds to wait before updating")
 
-(defvar spacemacs-auto-package/update-file
-  (expand-file-name "~/.emacs.d/private/last-package-update")
+(defvar spacemacs-auto-package-update/file
+  "~/.emacs.d/private/last-package-update"
   "The file to track update times using.")
 
-(defun spacemacs-auto-package-update/do-update ()
+(defun spacemacs-auto-package-update/do-update (update-file)
   "Do a package update and update the timestamp on the update fille."
   (message "Need to do automatic spacemacs package update")
   (configuration-layer/update-packages t)
-  (with-temp-file spacemacs-auto-package-update-file (save-buffer)))
+  (with-temp-file update-file (save-buffer)))
 
 (defun spacemacs-auto-package-update/check-and-apply-update ()
   "Apply an update if the file is too old or doesn't exist."
-  (when (or (not (file-exists-p spacemacs-auto-package/update-file))
-            (let* ((now (time-convert nil 'integer))
-                   (last-seen (time-convert
-                               (file-attribute-modification-time
-                                (file-attributes spacemacs-auto-package/update-file)) 'integer))
-                   (delta (- now last-seen)))
-              (>= delta spacemacs-auto-package/update-age)))
-    (spacemacs-auto-package-update/do-update)))
+  (let ((update-file (expand-file-name spacemacs-auto-package-update/file)))
+    (when (or (not (file-exists-p update-file))
+              (let* ((now (time-convert nil 'integer))
+                     (last-seen (time-convert (file-attribute-modification-time (file-attributes update-file)) 'integer))
+                     (delta (- now last-seen)))
+                (>= delta spacemacs-auto-package-update/age)))
+      (spacemacs-auto-package-update/do-update update-file))))
 
 (provide 'spacemacs-auto-package-update)
 ;;; spacemacs-auto-package-update.el ends here
